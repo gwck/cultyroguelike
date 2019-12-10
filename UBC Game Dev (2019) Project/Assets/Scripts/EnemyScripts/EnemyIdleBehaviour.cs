e2e5ee16eaca61c1a7ba4b55a5a2797e5a77e677
afroad <56143597+afroad@users.noreply.github.com>
2019-12-10 11:25:29 -0800

    Updated and fixed animations
    Made the damage animation work.
    fixed the running -> falling/landing animation.
    made bullet pink.

---
+++ UBC Game Dev (2019) Project/Assets/Scripts/EnemyScripts/EnemyIdleBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyIdleBehaviour : StateMachineBehaviour
+{
+    private Transform playerPos;
+    public float enemySight;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        canFollowPlayer(animator);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+
+    private void canFollowPlayer(Animator animator)
+    {
+        float x = animator.transform.position.x + enemySight;
+        if (x >= playerPos.transform.position.x)
+        {
+            animator.SetBool("isFollowing", true);
+        }
+    }
+}
