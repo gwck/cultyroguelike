13d93fd081e697d6b072f89fa43cfb8fa3ef06a2
gwck <40075885+gwck@users.noreply.github.com>
2020-01-16 10:56:10 -0800

    cleanup, exit door work

---
+++ UBC Game Dev (2019) Project/Assets/Scripts/Level/DoorTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("PlayerLayer"))
+        animator.SetTrigger("Open");
+    }
+}
---
+++ UBC Game Dev (2019) Project/Assets/Scripts/UI/VictoryMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class VictoryMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject victoryMenuUI;
+    
+    public void Restart()
+    {
+        victoryMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void Win()
+    {
+        victoryMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+        //        Debug.Log("Loading menu.");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game.");
+        Application.Quit();
+    }
+
+
+}
