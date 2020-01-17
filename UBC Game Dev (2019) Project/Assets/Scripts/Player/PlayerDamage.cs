1a322748417f910d2a851ca84c388b7d66bb4de0
gwck <40075885+gwck@users.noreply.github.com>
2020-01-16 19:49:27 -0800

    damage markers

---
+++ UBC Game Dev (2019) Project/Assets/Scripts/Player/PlayerDamage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamage : MonoBehaviour
+{
+    [SerializeField] public int maxHealth;
+    private int currentHealth;
+    [SerializeField] private GameObject[] healthTrackers;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("k"))
+        {
+            Debug.Log("took damage");
+            TakeDamage();
+        }
+    }
+
+    public void TakeDamage()
+    {
+        if (currentHealth > 0)
+        {
+            healthTrackers[--currentHealth].SetActive(false);
+        }
+    }
+}
