dab4b6ced608fdec879e9caa0717532adeea1dba
Kevin Chong <kevinchong314@gmail.com>
2020-02-26 15:57:57 -1000

    Scripts updated??
    I'm sure I pushed my stuff, but in the case that I didn't, I messed around with the scripts for items in general. The pickup system should still be there.

---
+++ UBC Game Dev (2019) Project/Assets/Scripts/Items/Item (superclass).cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private Transform playerPosition;
+
+    private PlayerController playerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPosition = GameObject.FindGameObjectWithTag("Player").transform;
+        playerController = playerPosition.gameObject.GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerPosition == null)
+        {
+            playerPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        }
+    }
+
+
+
+
+
+}
